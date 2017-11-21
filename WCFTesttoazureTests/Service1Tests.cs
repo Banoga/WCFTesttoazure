d046e2936f2983c493862194074b864a8e4e6c25
Banoga <bast0214@edu.easj.dk>
2017-11-21 13:44:37 +0100

    addtest

---
+++ WCFTesttoazureTests/Service1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFTesttoazure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFTesttoazure.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+           // Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataUsingDataContractTest()
+        {
+           //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Service1 addservice = new Service1();
+            double result = addservice.Add(4, 5.5);
+            Assert.AreEqual(9.5, result);
+        }
+    }
+}
